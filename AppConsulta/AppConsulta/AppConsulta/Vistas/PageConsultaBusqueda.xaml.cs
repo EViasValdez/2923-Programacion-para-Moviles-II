4610a033b48e5d2d6664c75cb6860b0d1c129910
EViasValdez <eduardoviasvaldez@yahoo.com.mx>
2023-08-28 11:48:26 -0600

    Add files via upload

---
+++ AppConsulta/AppConsulta/AppConsulta/Vistas/PageConsultaBusqueda.xaml.cs
@@ -0,0 +1,40 @@
+﻿using AppConsulta.Clases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppConsulta.Vistas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PageConsultaBusqueda : ContentPage
+    {
+        HTTPeticion peticion = new HTTPeticion();
+        
+        public PageConsultaBusqueda()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtBusqueda.Text))
+            {
+                DisplayAlert("Error", "Debe llenar el campo", "Aceptar");
+            }
+            else
+            {
+                string busqueda = txtBusqueda.Text;
+                if (peticion.ConsultaNombre(busqueda) == null)
+                {
+                    DisplayAlert("Error", "Fallo consulta al WebService", "Aceptar");
+                }
+                lstProductos.ItemsSource = peticion.ConsultaNombre(busqueda);
+            }
+        }
+    }
+}
