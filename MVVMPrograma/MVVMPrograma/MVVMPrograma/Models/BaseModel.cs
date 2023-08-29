6ce234da68409110cbf5c695f361081c04483e2a
EViasValdez <eduardoviasvaldez@yahoo.com.mx>
2023-08-28 20:18:43 -0600

    Add files via upload

---
+++ MVVMPrograma/MVVMPrograma/MVVMPrograma/Models/BaseModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace MVVMPrograma.Models
+{
+    public class BaseModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] String nombre = "")
+        {
+
+        }
+    }
+}
---
+++ MVVMPrograma/MVVMPrograma/MVVMPrograma/View/AlumnoView.xaml.cs
@@ -0,0 +1,29 @@
+﻿using MVVMPrograma.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MVVMPrograma.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AlumnoView : ContentPage
+    {
+        AlumnoViewModel contexto;
+        public AlumnoView()
+        {
+            InitializeComponent();
+            contexto = new AlumnoViewModel
+            {
+                ID = 1,
+                Nombre = "Eduardo"
+            };
+
+            BindingContext = contexto;
+        }
+    }
+}
