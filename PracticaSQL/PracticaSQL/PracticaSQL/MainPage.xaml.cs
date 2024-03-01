fe00bd4bdc6d426c1908f322848016043118ca8f
Eduardo <eduardoviasvaldez@yahoo.com.mx>
2024-03-01 09:50:10 -0600

    Add files via upload

---
+++ PracticaSQL/PracticaSQL/PracticaSQL/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using PracticaSQL.Modelos;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PracticaSQL
+{
+    public partial class MainPage : ContentPage
+    {
+        ModeloBD BasedeDatos = new ModeloBD();
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void BInsertar_Clicked(object sender, EventArgs s)
+        {
+            Alumno alumno = new Alumno
+            {
+                Nombre = ENombre.Text,
+                Apellidos = EApellidos.Text,
+                Matricula = EMatricula.Text,
+                Codigo = ECodigo.Text
+            };
+            BasedeDatos.Alumno.Add(alumno);
+            BasedeDatos.SaveChanges();
+        }
+        private void BConsultar_Clicked(object sender, EventArgs s)
+        {
+            {
+                var alumno = BasedeDatos.Alumno.ToList();
+            };
+        }
+        private void BInsertarGrupo_Clicked(object sender, EventArgs s)
+        {
+            {
+                Grupo grupo = new Grupo
+                {
+                    Nombre = EGrupo.Text
+                };
+
+                BasedeDatos.Grupo.Add(grupo);
+            };
+        }
+        private void BConsultarGrupo_Clicked(object sender, EventArgs s)
+        {
+            {
+                var grupos = BasedeDatos.Grupo.ToList();
+            };
+        }
+    }
+}
---
+++ PracticaSQL/PracticaSQL/PracticaSQL/Modelos/Alumno.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PracticaSQL.Modelos
+{
+    internal class Alumno
+    {
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+        public string Matricula { get; set; }
+        public string Codigo { get; set; }
+    }
+}
