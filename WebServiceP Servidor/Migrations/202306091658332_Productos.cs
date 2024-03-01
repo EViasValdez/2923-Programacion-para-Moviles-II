af3e8a0d5d7a45f4d23c9798524a9c9cd29ae54a
Eduardo <eduardoviasvaldez@yahoo.com.mx>
2024-03-01 10:03:36 -0600

    Add files via upload

---
+++ WebServiceP Servidor/Migrations/202306091658332_Productos.cs
@@ -0,0 +1,28 @@
+﻿namespace WebServiceP.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Productos : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Productoes",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Nombre = c.String(),
+                        Descripcion = c.String(),
+                        Precio = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Productoes");
+        }
+    }
+}
