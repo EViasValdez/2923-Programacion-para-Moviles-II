b32c67b6f6eecc46b27e0efac104962dd4b680e3
Eduardo <eduardoviasvaldez@yahoo.com.mx>
2024-03-01 10:32:37 -0600

    Add files via uplo

---
+++ AplicacionMAUI/AplicacionMAUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AplicacionMAUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AplicacionMAUI/AplicacionMAUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AplicacionMAUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
