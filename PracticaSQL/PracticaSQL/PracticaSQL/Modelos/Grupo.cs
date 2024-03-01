ba6639c35feb8a9914d3ee068a7b706402d05288
Eduardo <eduardoviasvaldez@yahoo.com.mx>
2024-03-01 14:55:32 -0600

    Add files via upload

---
+++ PracticaSQL/PracticaSQL/PracticaSQL/Modelos/Grupo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PracticaSQL.Modelos
+{
+    internal class Grupo
+    {
+        public int ID { get; set; }
+        public String Nombre { get; set; }
+        public String Matricula { get; set; }
+        public int GrupoID { get; set; }
+        public virtual Grupo grupo { get; set; }
+    }
+}
