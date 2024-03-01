9182c42dcdae4199172f8fb437c56f003153d418
Eduardo <eduardoviasvaldez@yahoo.com.mx>
2024-03-01 09:40:52 -0600

    Add files via upload

---
+++ AppConsulta/AppConsulta/AppConsulta/Clases/Productos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppConsulta.Clases
+{
+    public class Productos
+    {
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public int Precio { get; set; }
+    }
+}
---
+++ MVVMPrograma/MVVMPrograma/MVVMPrograma/Models/AlumnoModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MVVMPrograma.Models
+{
+    public class AlumnoModel
+    {
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+    }
+}
