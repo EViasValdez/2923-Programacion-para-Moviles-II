7e6b5ec12fa25258aba61889b94bdc8a2512c353
Eduardo <eduardoviasvaldez@yahoo.com.mx>
2024-03-12 09:35:51 -0600

    Add files via upload

--- PracticaSQL/PracticaSQL/PracticaSQL/obj/Release/netstandard2.1/PracticaSQL.AssemblyInfo.cs
+++ PracticaSQL/PracticaSQL/PracticaSQL/obj/Release/netstandard2.1/PracticaSQL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PracticaSQL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef0ddd62b2328e033b35152aaaa31f34cfc6770e")]
 [assembly: System.Reflection.AssemblyProductAttribute("PracticaSQL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PracticaSQL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- QR/QR/obj/Release/netstandard2.0/QR.AssemblyInfo.cs
+++ QR/QR/obj/Release/netstandard2.0/QR.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QR")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef0ddd62b2328e033b35152aaaa31f34cfc6770e")]
 [assembly: System.Reflection.AssemblyProductAttribute("QR")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QR")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
