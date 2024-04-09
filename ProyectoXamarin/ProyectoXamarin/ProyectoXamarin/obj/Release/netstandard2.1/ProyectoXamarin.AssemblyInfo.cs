7b58e5f9eb179062c431f6b31f8ccb519883eff6
Eduardo <eduardoviasvaldez@yahoo.com.mx>
2024-04-09 16:35:18 -0600

    Add files via upload

--- ProyectoXamarin/ProyectoXamarin/ProyectoXamarin/obj/Release/netstandard2.1/ProyectoXamarin.AssemblyInfo.cs
+++ ProyectoXamarin/ProyectoXamarin/ProyectoXamarin/obj/Release/netstandard2.1/ProyectoXamarin.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoXamarin")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9e00e6465c4e9ad6d7ed6227461f38d9d499a03")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+004e3f95cd06da3697884948c783e447f759d899")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoXamarin")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoXamarin")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
