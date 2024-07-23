a00de6f5ce0616fe89e5e60d8f3a1fa0cd7fb63d
Eduardo <eduardoviasvaldez@yahoo.com.mx>
2024-07-23 09:23:17 -0600

    Add files via upload

--- AppConsulta/AppConsulta/AppConsulta/obj/Release/netstandard2.0/AppConsulta.AssemblyInfo.cs
+++ AppConsulta/AppConsulta/AppConsulta/obj/Release/netstandard2.0/AppConsulta.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppConsulta")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a324362f6ba44274b605cdad11c1135628e7da2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09090efd55560d4eb722213f961030c548053f8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppConsulta")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppConsulta")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
